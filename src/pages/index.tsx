82d9f470eea30169f43de5d7717eed46b467cb08
warrenau <onsenkame@gmail.com>
2023-02-08 02:26:40 -0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>To What Ends</title>
+        <meta name="description" content="A YOU centric choose your own adventure game" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+
+      </main>
+    </>
+  )
+}
